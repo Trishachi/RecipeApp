bfc11f05a9d6597669c94b99b5def6a0765f1926
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-10-24 12:18:21 -0600

    Media Queries for small screens

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -21,8 +21,8 @@ class Recipe extends React.Component {
   };
 
   getIngredients = (arr) => {
-    return arr.join(", ")
-  }
+    return arr.join(", ");
+  };
 
   render() {
     const recipe = this.state.activeRecipe;
@@ -41,49 +41,57 @@ class Recipe extends React.Component {
               <div className="col-md-6">
                 <h3 className="active-recipe__title">{recipe.title}</h3>
                 <h4 className="active-recipe__publisher">
-                  By: <span><strong>{recipe.publisher}</strong></span>
-                </h4>                
+                  By:{" "}
+                  <span>
+                    <strong>{recipe.publisher}</strong>
+                  </span>
+                </h4>
                 <p className="active-recipe__description">
                   Description:{" "}
-                  <span>            
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras condimentum, lorem et convallis pellentesque, leo lorem pretium nulla, ac commodo mi quam sed massa. Nam et lorem convallis, dignissim arcu vel, placerat nulla.
+                  <span>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Cras condimentum, lorem et convallis pellentesque, leo lorem
+                    pretium nulla, ac commodo mi quam sed massa. Nam et lorem
+                    convallis, dignissim arcu vel, placerat nulla.
                   </span>
                 </p>
                 <p className="active-recipe__Ingredients">
                   Ingredients:{" "}
-                  <span>            
-                    { this.getIngredients(recipe.ingredients)} 
-                  </span>
+                  <span>{this.getIngredients(recipe.ingredients)}</span>
                 </p>
                 <p className="active-recipe__website">
                   Website:{" "}
                   <span>
-                    <a href={recipe.publisher_url} target="_blank">View Source</a>
+                    <a href={recipe.publisher_url} target="_blank">
+                      View Source
+                    </a>
                   </span>
-                </p>   
-                
-              </div>              
+                </p>
+              </div>
             </div>
             <div className="row">
-                <div className="col-md-12 active-recipe__video">
-                  <div className="">
-                    <h4>Video Instructions</h4>
-                    <p>Below is a video altenative of the {recipe.title} recipe if you prefer to learn by wacthing videos rather than reading. </p>
-                    <iframe
-                      width="560"
-                      height="315"
-                      src={recipe.video_directions}
-                      frameBorder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                      allowFullScreen
-                      title="video"
-                    ></iframe>
-                    <button className="active-recipe__button">
-                      <Link to="/">Go Home</Link>
-                    </button>
-                  </div>                  
-                </div>                
+              <div className="col-md-12 active-recipe__video">
+                <div className="">
+                  <h4>Video Instructions</h4>
+                  <p>
+                    Below is a video altenative of the {recipe.title} recipe if
+                    you prefer to learn by wacthing videos rather than reading.{" "}
+                  </p>
+                  <iframe
+                    width="560"
+                    height="315"
+                    src={recipe.video_directions}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="video"
+                  ></iframe>
+                  <button className="active-recipe__button">
+                    <Link to="/">Go Home</Link>
+                  </button>
+                </div>
               </div>
+            </div>
           </div>
         )}
         <footer>
